f71997b8792438a07284c4e42da8f5e5cd70de44
Thomas Clermont <fhs49807@fh-salzburg.ac.at>
2024-10-28 00:06:50 +0100

    commandLineStartupRunner

--- springboot-master/springboot-master/src/main/java/at/ac/fhsalzburg/swd/spring/startup/CommandLineAppStartupRunner.java
+++ springboot-master/springboot-master/src/main/java/at/ac/fhsalzburg/swd/spring/startup/CommandLineAppStartupRunner.java
@@ -1,11 +1,15 @@
 package at.ac.fhsalzburg.swd.spring.startup;
 
 import at.ac.fhsalzburg.swd.spring.model.Customer;
+import at.ac.fhsalzburg.swd.spring.model.User;
 import at.ac.fhsalzburg.swd.spring.repository.CustomerRepository;
 import at.ac.fhsalzburg.swd.spring.services.MediaServiceInterface;
 import at.ac.fhsalzburg.swd.spring.services.OrderServiceInterface;
 import at.ac.fhsalzburg.swd.spring.services.ProductServiceInterface;
 import at.ac.fhsalzburg.swd.spring.services.UserServiceInterface;
+
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
@@ -35,17 +39,18 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
     @Transactional
     public void run(String... args) throws Exception {
 
-        //		if (userService.getByUsername("admin") != null) return;
-        //
-        //		userService.addUser("admin", "Admin", "admin@gmail.com", "123", new Date(123), "123", "ADMIN");
-        //
-        //		productService.addProduct("firstArticle", 2.00f);
-        //        User user = userService.getAll().iterator().next();	// User user = userService.getByUsername
-        //        ("admin");
-        //        user.setCredit(100l);
-        //        user = userService.getByUsername("admin");
-        //        orderService.addOrder(new Date(), user, productService.getAll());
 
+    	if (userService.getByUsername("admin")!=null) return; // data already exists -> return
+
+    	userService.addUser("admin", "Administrator", "admin@work.org", "123", new Date(123), "admin","ADMIN");
+
+        productService.addProduct("first product", 3.30f);
+        User user = userService.getAll().iterator().next();
+        user.setCredit(100l);
+        user = userService.getByUsername("admin");
+        orderService.addOrder(new Date(), user, productService.getAll());
+    	
+    	
         Customer customer = new Customer(null, "Student", 5, "Test Name");
         customerRepository.save(customer);
 
