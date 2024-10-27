ac9a9551f49575038580b74a538135b97b51d03d
marioneubacher1 <fhs49993@fh-salzburg.ac.at>
2024-10-27 17:30:42 +0100

    Update CommandLineAppStartupRunner

--- springboot-master/springboot-master/src/main/java/at/ac/fhsalzburg/swd/spring/startup/CommandLineAppStartupRunner.java
+++ springboot-master/springboot-master/src/main/java/at/ac/fhsalzburg/swd/spring/startup/CommandLineAppStartupRunner.java
@@ -7,27 +7,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import at.ac.fhsalzburg.swd.spring.model.Customer;
-import at.ac.fhsalzburg.swd.spring.model.User;
 import at.ac.fhsalzburg.swd.spring.repository.CustomerRepository;
 import at.ac.fhsalzburg.swd.spring.services.CustomerServiceInterface;
 import at.ac.fhsalzburg.swd.spring.services.OrderServiceInterface;
-import at.ac.fhsalzburg.swd.spring.services.ProductServiceInterface;
-import at.ac.fhsalzburg.swd.spring.services.UserServiceInterface;
 
 import java.sql.Date;
 
 @Profile("!test")
 @Component
 public class CommandLineAppStartupRunner implements CommandLineRunner {
-
-	// @Autowired
-	// UserServiceInterface userService;
-	//
-	// @Autowired
-	// ProductServiceInterface productService;
-	//
-	// @Autowired
-	// OrderServiceInterface orderService;
+	@Autowired
+	OrderServiceInterface orderService;
 
 	@Autowired
 	CustomerServiceInterface customerService;
@@ -52,10 +42,31 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
 
 		// -------------------------------------------------------------
 
-		Customer customer = new Customer(1, null, "Student", 5, "Test Name");
-		customerRepository.save(customer);
+		//Customer customer = new Customer(1, null, "Student", 5, "Test Name");
+		//customerRepository.save(customer);
 
-		System.out.println("Customer created: " + customer.getName() + " with ID: " + customer.getCustomerID());
+		//System.out.println("Customer created: " + customer.getName() + " with ID: " + customer.getCustomerID());
+
+		// Create: Erstelle ein Customer-Objekt und speichere es
+		Customer newCustomer = new Customer(1, Date.valueOf("1990-01-01"), "Standard", 5, "John Doe");
+		customerRepository.save(newCustomer);
+		System.out.println("Customer created: " + newCustomer);
+
+		// Read: Lade einen Customer anhand seiner ID
+		Customer customer = customerRepository.findById(1).orElse(null);
+		System.out.println("Customer read: " + customer);
+
+		// Update: Ändere die Attribute des geladenen Customer und speichere das
+		// aktualisierte Objekt
+		if (customer != null) {
+			customer.setLoanLimit(10);
+			customerRepository.save(customer);
+			System.out.println("Customer updated: " + customer);
+		}
+
+		// Delete: Lösche den Customer anhand seiner ID
+		customerRepository.deleteById(1);
+		System.out.println("Customer deleted with ID 1");
 
 	}
 
@@ -76,34 +87,4 @@ public class CommandLineAppStartupRunner implements CommandLineRunner {
 	// return media
 
 	// create invoice
-
-	@Component
-	public class CommandLineStartupRunner implements CommandLineRunner {
-		@Autowired
-		private CustomerRepository customerRepository;
-
-		@Override
-		public void run(String... args) throws Exception {
-			// Create: Erstelle ein Customer-Objekt und speichere es
-			Customer newCustomer = new Customer(1, Date.valueOf("1990-01-01"), "Standard", 5, "John Doe");
-			customerRepository.save(newCustomer);
-			System.out.println("Customer created: " + newCustomer);
-
-			// Read: Lade einen Customer anhand seiner ID
-			Customer customer = customerRepository.findById(1).orElse(null);
-			System.out.println("Customer read: " + customer);
-
-			// Update: Ändere die Attribute des geladenen Customer und speichere das
-			// aktualisierte Objekt
-			if (customer != null) {
-				customer.setLoanLimit(10);
-				customerRepository.save(customer);
-				System.out.println("Customer updated: " + customer);
-			}
-
-			// Delete: Lösche den Customer anhand seiner ID
-			customerRepository.deleteById(1);
-			System.out.println("Customer deleted with ID 1");
-		}
-	}
 }
